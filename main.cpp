af6be3cb3bd347077fd3400b52dd41922bfeac01
SamuelFontes <samuguel@hotmail.com.br>
2025-08-02 15:34:04 -0500

    Fix typo in game executable path

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     // Define the game executable and OBB paths, dirty for now to get it working
-    const char *GAME_EXECUTABLE_RELATIVE_PATH = "/home/sfontes/Documents/game/extacted/lib/arm64-v8a/libUE4.so";
+    const char *GAME_EXECUTABLE_RELATIVE_PATH = "/home/sfontes/Documents/game/extracted/lib/arm64-v8a/libUE4.so";
     const char *GAME_OBB_PATH = "/home/sfontes/Documents/game/com.Armature.VR4";
 
     std::cout << "Hello, world!" << std::endl;
